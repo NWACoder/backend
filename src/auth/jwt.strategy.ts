750ae08cf3a3210f3935bb44e15d37d8397c5133
xamthor <miguel@mhernandez.me>
2021-07-04 01:13:10 -0500

    rm unneeded imports

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { jwtConstants } from './constants';
 
 import * as dotenv from 'dotenv';
 import { ConfigService } from '@nestjs/config';
