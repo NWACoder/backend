e9c096397d1cfdd7ded1410a3ffa3cb89162da4f
xamthor <miguel@mhernandez.me>
2021-07-13 09:54:13 -0500

    mail ...

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 import { MailService } from './mail.service';
 
 @Controller("mail")
 export class MailController {
 	constructor(private readonly mailService: MailService) {}
 
+	@ApiExcludeEndpoint()	
   	@Get("example")
 	findAll() {
 		return this.mailService.example();
