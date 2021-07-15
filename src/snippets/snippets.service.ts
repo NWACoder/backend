555aaae2db1f28cd47525d05b3abaf9b79f99bfe
Jesse <moore.jesse@gmail.com>
2021-07-15 13:50:34 -0500

    added get snippets by user

--- src/snippets/snippets.service.ts
+++ src/snippets/snippets.service.ts
@@ -8,38 +8,54 @@ import { User } from 'src/users/user.schema';
 
 @Injectable()
 export class SnippetsService {
+  constructor(
+    @InjectModel(Snippet.name) private snippetModel: Model<SnippetDocument>,
+  ) {}
 
-	constructor(@InjectModel(Snippet.name) private snippetModel: Model<SnippetDocument>) {}
+  async create(createSnippetDto: CreateSnippetDto): Promise<Snippet> {
+    const createdSnippet = new this.snippetModel(createSnippetDto);
 
-	async create(createSnippetDto: CreateSnippetDto): Promise<Snippet> {
-		const createdSnippet = new this.snippetModel(createSnippetDto);
-		
-    	return createdSnippet.save();
-	}
+    return createdSnippet.save();
+  }
 
-	async findAll(): Promise<Snippet[]> {
-		return this.snippetModel.find({ public: true}).populate('tags');
-	}
+  async findAll(): Promise<Snippet[]> {
+    return this.snippetModel.find({ public: true }).populate('tags');
+  }
 
-	async findByUser(user_id: User): Promise<Snippet[]> {
-		return this.snippetModel.find({ user_id }).populate('tags');
-	}
+  async findByUser(user_id: User): Promise<Snippet[]> {
+    return this.snippetModel
+      .find({ user_id })
+      .populate('tags')
+      .populate('user_id', 'username')
+      .populate('items', 'name');
+  }
 
+  async getLatest(size: number): Promise<Snippet[]> {
+    return this.snippetModel
+      .find({ public: true })
+      .populate('tags')
+      .limit(+size)
+      .sort({ $natural: -1 });
+  }
 
+  async findById(id: string): Promise<Snippet> {
+    return this.snippetModel
+      .findById(id)
+      .populate('user_id', 'username')
+      .populate('items')
+      .populate('tags');
+  }
 
-	async getLatest(size: number): Promise<Snippet[]> {
-		return this.snippetModel.find({ public: true}).populate('tags').limit(+size).sort({$natural:-1});
-	}
+  async update(
+    id: string,
+    updateSnippetDto: UpdateSnippetDto,
+  ): Promise<Snippet> {
+    return this.snippetModel.findByIdAndUpdate(id, updateSnippetDto, {
+      new: true,
+    });
+  }
 
-	async findById(id: string): Promise<Snippet> {
-		return this.snippetModel.findById(id).populate('user_id', "username").populate('items').populate('tags');
-	}
-
-	async update(id: string, updateSnippetDto: UpdateSnippetDto): Promise<Snippet> {
-    	return this.snippetModel.findByIdAndUpdate(id,updateSnippetDto, {new: true});
-	}
-
-	async remove(id: string) {
-		return this.snippetModel.findByIdAndDelete(id);
-	}
+  async remove(id: string) {
+    return this.snippetModel.findByIdAndDelete(id);
+  }
 }
