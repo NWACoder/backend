23daab5003f42d666442a8f7dbd7251ce9e9e2a3
xamthor <miguel@mhernandez.me>
2021-07-16 15:43:57 -0500

    certBuild dist

--- certBuilder.js
+++ certBuilder.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 require('dotenv').config()
 
 try {
-		const data = fs.writeFileSync('ca-certificate.cer', process.env.CA_CERT )
+	const data = fs.writeFileSync('dist/ca-certificate.cer', process.env.CA_CERT )
 } catch (err) {
 	console.error(err)
 }
